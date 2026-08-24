028152d0a34e95c4edcc1cd8e235c2eec1e01465
nitheshraj <ipnitheshraj21@gmail.com>
2026-08-24 13:20:26 +0000

    template setup.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,2 @@
 // src/index.js
 import "./styles.css";
-import { greeting } from "./greeting.js";
-
-console.log(greeting);
-
-
-// src/index.js
-// import odinImage from "./Screenshot 2026-08-16 100645.png";
-
-// const image = document.createElement("img");
-// image.src = odinImage;
-
-// document.body.appendChild(image);
