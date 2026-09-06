a381fcc73282eb84912e70d94b25aac8b24d36da
nitheshraj <ipnitheshraj21@gmail.com>
2026-09-06 17:06:41 +0000

    added prettier.

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,26 @@
 import { defineConfig } from "eslint/config";
 import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default defineConfig([
-	{
-		files: ["**/*.js"],
-		plugins: {
-			js,
-		},
-		extends: ["js/recommended"],
-		rules: {
-			"no-unused-vars": "warn",
-			"no-undef": "warn",
-		},
-	},
+  {
+    ignores: ["dist/**"],
+  },
+
+  {
+    files: ["**/*.js"],
+
+    plugins: {
+      js,
+    },
+
+    extends: ["js/recommended"],
+
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
+    },
+  },
+
+  eslintConfigPrettier,
 ]);
